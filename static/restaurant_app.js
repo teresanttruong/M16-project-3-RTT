a166e2a809f8b59235de50c397a938f5c66f34a1
TPaxton97 <victorapaxton@gmail.com>
2023-08-31 20:00:57 -0400

    data folder created, outline for js file written

---
+++ static/restaurant_app.js
@@ -0,0 +1,22 @@
+//Establish reference to data
+
+
+
+//Create base map and layers to hold markers
+//basemap
+
+//layer for...
+
+//layer for...
+
+//layer for...
+
+
+
+//Create markers
+//color based on type of restaurant(?)
+
+
+
+//Get data and call createMarkers
+
